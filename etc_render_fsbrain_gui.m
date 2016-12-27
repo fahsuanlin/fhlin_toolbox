97dd51c36300f8201754c92cb7510b1ac7ae54a6
fahsuanlin <fhlin@ntu.edu.tw>
2016-12-27 21:17:03 +0800

    122716

---
+++ etc_render_fsbrain_gui.m
@@ -0,0 +1,369 @@
+function varargout = etc_render_fsbrain_gui(varargin)
+% ETC_RENDER_FSBRAIN_GUI MATLAB code for etc_render_fsbrain_gui.fig
+%      ETC_RENDER_FSBRAIN_GUI, by itself, creates a new ETC_RENDER_FSBRAIN_GUI or raises the existing
+%      singleton*.
+%
+%      H = ETC_RENDER_FSBRAIN_GUI returns the handle to a new ETC_RENDER_FSBRAIN_GUI or the handle to
+%      the existing singleton*.
+%
+%      ETC_RENDER_FSBRAIN_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in ETC_RENDER_FSBRAIN_GUI.M with the given input arguments.
+%
+%      ETC_RENDER_FSBRAIN_GUI('Property','Value',...) creates a new ETC_RENDER_FSBRAIN_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before etc_render_fsbrain_gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to etc_render_fsbrain_gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help etc_render_fsbrain_gui
+
+% Last Modified by GUIDE v2.5 27-Dec-2016 16:39:15
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @etc_render_fsbrain_gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @etc_render_fsbrain_gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before etc_render_fsbrain_gui is made visible.
+function etc_render_fsbrain_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to etc_render_fsbrain_gui (see VARARGIN)
+
+% Choose default command line output for etc_render_fsbrain_gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes etc_render_fsbrain_gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = etc_render_fsbrain_gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on slider movement.
+function slider_timeVec_Callback(hObject, eventdata, handles)
+% hObject    handle to slider_timeVec (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+global etc_render_fsbrain
+time=get(hObject,'Value');
+[dummy, etc_render_fsbrain.overlay_stc_timeVec_idx]=min(abs(etc_render_fsbrain.overlay_stc_timeVec-time));
+
+if(~iscell(etc_render_fsbrain.overlay_value))
+    etc_render_fsbrain.overlay_value=etc_render_fsbrain.overlay_stc(:,etc_render_fsbrain.overlay_stc_timeVec_idx);
+else
+    for h_idx=1:length(etc_render_fsbrain.overlay_value)
+        etc_render_fsbrain.overlay_value{h_idx}=etc_render_fsbrain.overlay_stc_hemi{h_idx}(:,etc_render_fsbrain.overlay_stc_timeVec_idx);
+    end;
+end;
+
+
+%update time index edit
+h=findobj('tag','edit_timeVec');
+set(h,'value',time);
+set(h,'string',sprintf('%1.1f',time));
+
+if(isempty(etc_render_fsbrain.overlay_stc_timeVec))
+    fprintf('showing STC at time index [%d] (sample)\n',etc_render_fsbrain.overlay_stc_timeVec_idx);
+else
+    fprintf('showing STC at time [%2.2f] ms\n',etc_render_fsbrain.overlay_stc_timeVec(etc_render_fsbrain.overlay_stc_timeVec_idx));
+end;
+
+etc_render_fsbrain_handle('draw_stc');
+etc_render_fsbrain_handle('redraw');
+
+
+
+
+% --- Executes during object creation, after setting all properties.
+function slider_timeVec_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider_timeVec (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+global etc_render_fsbrain
+if(~isempty(etc_render_fsbrain.overlay_stc))    
+    if(~isempty(etc_render_fsbrain.overlay_stc_timeVec))
+        set(hObject,'Min',min(etc_render_fsbrain.overlay_stc_timeVec));
+        set(hObject,'Max',max(etc_render_fsbrain.overlay_stc_timeVec));
+        if(isfield(etc_render_fsbrain,'overlay_stc_timeVec_idx'));
+            set(hObject,'value',etc_render_fsbrain.overlay_stc_timeVec(etc_render_fsbrain.overlay_stc_timeVec_idx));
+        end;
+    else
+        etc_render_fsbrain.overlay_stc_timeVec=[1:size(etc_render_fsbrain.overlay_stc,2)];
+        set(hObject,'Min',min(etc_render_fsbrain.overlay_stc_timeVec));
+        set(hObject,'Max',max(etc_render_fsbrain.overlay_stc_timeVec));
+        if(isfield(etc_render_fsbrain,'overlay_stc_timeVec_idx'));
+            set(hObject,'value',etc_render_fsbrain.overlay_stc_timeVec(etc_render_fsbrain.overlay_stc_timeVec_idx));
+        end;        
+    end;
+end;
+
+
+% --- Executes on button press in checkbox_show_overlay.
+function checkbox_show_overlay_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox_show_overlay (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox_show_overlay
+global etc_render_fsbrain
+etc_render_fsbrain.overlay_flag_render=get(hObject,'value');
+etc_render_fsbrain_handle('redraw');
+
+function edit_threshold_min_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_threshold_min (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_threshold_min as text
+%        str2double(get(hObject,'String')) returns contents of edit_threshold_min as a double
+global etc_render_fsbrain
+mm=str2double(get(hObject,'string'));
+mx=max(etc_render_fsbrain.overlay_threshold);
+etc_render_fsbrain.overlay_threshold=[mm,mx];
+etc_render_fsbrain_handle('redraw');
+fprintf('redraw DONE!\n');
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_threshold_min_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_threshold_min (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+global etc_render_fsbrain
+set(hObject,'string',sprintf('%1.1f',min(etc_render_fsbrain.overlay_threshold)));
+
+
+
+
+function edit_threshold_max_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_threshold_max (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_threshold_max as text
+%        str2double(get(hObject,'String')) returns contents of edit_threshold_max as a double
+global etc_render_fsbrain
+mx=str2double(get(hObject,'string'));
+mm=min(etc_render_fsbrain.overlay_threshold);
+etc_render_fsbrain.overlay_threshold=[mm,mx];
+etc_render_fsbrain_handle('redraw');
+fprintf('redraw DONE!\n');
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_threshold_max_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_threshold_max (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+global etc_render_fsbrain
+set(hObject,'string',sprintf('%1.1f',max(etc_render_fsbrain.overlay_threshold)));
+
+
+% --- Executes during object creation, after setting all properties.
+function text_timeVec_min_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text_timeVec_min (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global etc_render_fsbrain
+if(~isempty(etc_render_fsbrain.overlay_stc))    
+    if(isempty(etc_render_fsbrain.overlay_stc_timeVec))
+        etc_render_fsbrain.overlay_stc_timeVec=[1:size(etc_render_fsbrain.overlay_stc,2)];
+    end;
+    set(hObject,'String',sprintf('%1.0f',min(etc_render_fsbrain.overlay_stc_timeVec)));
+    set(hObject,'visible',1);
+else
+    set(hObject,'visible',0);
+end;
+
+
+% --- Executes during object creation, after setting all properties.
+function text_timeVec_max_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text_timeVec_max (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global etc_render_fsbrain
+if(~isempty(etc_render_fsbrain.overlay_stc))    
+    if(isempty(etc_render_fsbrain.overlay_stc_timeVec))
+        etc_render_fsbrain.overlay_stc_timeVec=[1:size(etc_render_fsbrain.overlay_stc,2)];
+    end;
+    set(hObject,'String',sprintf('%1.0f',max(etc_render_fsbrain.overlay_stc_timeVec)));
+    set(hObject,'visible',1);
+else
+    set(hObject,'visible',0);
+end;
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox_show_overlay_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox_show_overlay (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global etc_render_fsbrain
+set(hObject,'value',etc_render_fsbrain.overlay_flag_render);
+
+
+
+function edit_timeVec_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_timeVec (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_timeVec as text
+%        str2double(get(hObject,'String')) returns contents of edit_timeVec as a double
+global etc_render_fsbrain
+time=str2double(get(hObject,'string'));
+[dummy, etc_render_fsbrain.overlay_stc_timeVec_idx]=min(abs(etc_render_fsbrain.overlay_stc_timeVec-time));
+
+if(~iscell(etc_render_fsbrain.overlay_value))
+    etc_render_fsbrain.overlay_value=etc_render_fsbrain.overlay_stc(:,etc_render_fsbrain.overlay_stc_timeVec_idx);
+else
+    for h_idx=1:length(etc_render_fsbrain.overlay_value)
+        etc_render_fsbrain.overlay_value{h_idx}=etc_render_fsbrain.overlay_stc_hemi{h_idx}(:,etc_render_fsbrain.overlay_stc_timeVec_idx);
+    end;
+end;
+
+
+%update time index edit
+h=findobj('tag','slider_timeVec');
+set(h,'value',time);
+
+if(isempty(etc_render_fsbrain.overlay_stc_timeVec))
+    fprintf('showing STC at time index [%d] (sample)\n',etc_render_fsbrain.overlay_stc_timeVec_idx);
+else
+    fprintf('showing STC at time [%2.2f] ms\n',etc_render_fsbrain.overlay_stc_timeVec(etc_render_fsbrain.overlay_stc_timeVec_idx));
+end;
+
+etc_render_fsbrain_handle('draw_stc');
+etc_render_fsbrain_handle('redraw');
+
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_timeVec_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_timeVec (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+global etc_render_fsbrain
+set(hObject,'string',etc_render_fsbrain.overlay_stc_timeVec_idx);
+
+
+% --- Executes during object creation, after setting all properties.
+function text11_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global etc_render_fsbrain
+set(hObject,'string',etc_render_fsbrain.overlay_stc_timeVec_unit);
+
+
+% --- Executes on button press in checkbox_show_colorbar.
+function checkbox_show_colorbar_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox_show_colorbar (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox_show_colorbar
+global etc_render_fsbrain
+set(etc_render_fsbrain.fig_brain,'currentchar','c');
+etc_render_fsbrain_handle('kb','cc','c');
+
+% --- Executes during object creation, after setting all properties.
+function checkbox_show_colorbar_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox_show_colorbar (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global etc_render_fsbrain
+
+set(hObject,'value',1);
+if(isfield(etc_render_fsbrain,'h_colorbar_pos'))
+    if(isempty(etc_render_fsbrain.h_colorbar_pos))
+        set(hObject,'value',0);
+    end;
+end;
+
+
+
+function edit_smooth_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_smooth (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_smooth as text
+%        str2double(get(hObject,'String')) returns contents of edit_smooth as a double
+global etc_render_fsbrain
+etc_render_fsbrain.overlay_smooth=str2double(get(hObject,'string'));
+etc_render_fsbrain_handle('redraw');
+
+% --- Executes during object creation, after setting all properties.
+function edit_smooth_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_smooth (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+global etc_render_fsbrain
+set(hObject,'string',sprintf('%1.0f',etc_render_fsbrain.overlay_smooth));
---
+++ etc_render_topo.m
@@ -0,0 +1,328 @@
+function h=etc_render_topo(varargin)
+%
+% etc_render_topo    rendering the topology of multi-electrode event-related fields/potentials.
+% h = etc_render_topo([option1 option1_value, ...]);
+%
+% h: the handle of the patch object
+%
+% fhlin@dec 26 2016
+%
+
+h=[];
+
+vol_vertex=[];
+vol_face=[];
+vol_vertex_hemi={};
+vol_face_hemi={};
+
+flag_curv=0;
+curv=[];
+
+%color 
+default_solid_color=[256 180 100]./256;
+%curv_pos_color=[1 1 1].*0.4;
+%curv_neg_color=[1 1 1].*0.7;
+
+bg_color=[1 1 1]; %figure background color;
+
+tmp=jet(256);
+topo_cmap=tmp(129:end,:);
+% overlay_cmap=autumn(80); %overlay colormap;
+%topo_cmap_neg=winter(80); %overlay colormap;
+topo_cmap_neg=flipud(tmp(1:128,:));
+
+%overlay
+topo_value=[];
+topo_stc=[];
+topo_stc_hemi=[];
+topo_stc_lim=[];
+topo_stc_timeVec=[];
+topo_stc_timeVec_unit='';
+topo_stc_timeVec_idx=[];
+topo_vertex=[];
+topo_vertex_hemi=[];
+
+topo_smooth=5;
+topo_threshold=[];
+topo_value_flag_pos=0;
+topo_value_flag_neg=0;
+topo_regrid_flag=1;
+topo_regrid_zero_flag=0;
+topo_flag_render=0;
+
+topo_exclude_fstem='';
+topo_exclude=[];
+
+topo_include_fstem='';
+topo_include=[];
+
+%stc time course
+flag_hold_fig_stc_timecourse=0;
+
+%cluster file
+cluster_file={};
+
+%etc
+alpha=1;
+view_angle=[]; 
+
+flag_redraw=0;
+flag_camlight=1;
+flag_colorbar=0;
+
+for idx=1:length(varargin)/2
+    option=varargin{idx*2-1};
+    option_value=varargin{idx*2};
+    switch lower(option)
+        case 'vol_face'
+            vol_face=option_value;
+        case 'vol_face_hemi'
+            vol_face_hemi=option_value;
+        case 'vol_vertex'
+            vol_vertex=option_value;
+        case 'vol_vertex_hemi'
+            vol_vertex_hemi=option_value;
+        case 'topo_vertex'
+            topo_vertex=option_value;
+        case 'topo_flag_render'
+            topo_flag_render=option_value;
+        case 'topo_value'
+            topo_value=option_value;
+        case 'topo_value_hemi'
+            topo_value_hemi=option_value;
+        case 'topo_smooth'
+            topo_smooth=option_value;            
+        case 'topo_threshold'
+            topo_threshold=option_value;
+        case 'topo_cmap'
+            topo_cmap=option_value;
+        case 'topo_cmap_neg'
+            topo_cmap_neg=option_value;
+        case 'topo_regrid_flag'
+            topo_regrid_flag=option_value;
+        case 'topo_regrid_zero_flag'
+            topo_regrid_zero_flag=option_value;           
+        case 'topo_exclude'
+            topo_exclude=option_value;
+        case 'topo_exclude_fstem'
+            topo_exclude_fstem=option_value;
+        case 'topo_include'
+            topo_include=option_value;
+        case 'topo_include_fstem'
+            topo_include_fstem=option_value;    
+        case 'topo_stc'
+            topo_stc=option_value;       
+        case 'topo_stc_lim'
+            topo_stc_lim=option_value;
+        case 'topo_stc_timevec'
+            topo_stc_timeVec=option_value;
+        case 'topo_stc_timevec_unit'
+            topo_stc_timeVec_unit=option_value;            
+            
+            
+            
+            
+        case 'default_solid_color'
+            default_solid_color=option_value;
+        case 'curv_pos_color'
+            curv_pos_color=option_value;
+        case 'curv_neg_color'
+            curv_neg_color=option_value;
+        case 'overlay_value'
+            overlay_value=option_value;       
+        case 'overlay_vertex'
+            overlay_vertex=option_value;
+
+        
+        case 'cluster_file'
+            cluster_file=option_value;
+        case 'alpha'
+            alpha=option_value;
+        case 'flag_redraw'
+            flag_redraw=option_value;
+        case 'flag_camlight'
+            flag_camlight=option_value;
+        case 'flag_colorbar'
+            flag_colorbar=option_value;
+        case 'flag_hold_fig_stc_timecourse'
+            flag_hold_fig_stc_timecourse=option_value;
+        case 'view_angle'
+            view_angle=option_value;
+        case 'bg_color'
+            bg_color=option_value;
+        otherwise
+            fprintf('unknown option [%s]...\n',option);
+            return;
+    end;
+end
+
+%get the overlay value from STC at the largest power instant, if it is not specified.
+if(isempty(topo_value)&~isempty(topo_stc))
+    
+    [tmp,topo_stc_timeVec_idx]=max(sum(topo_stc.^2,1));
+    topo_value=topo_stc(:,topo_stc_timeVec_idx);
+end;
+
+
+%0: solid color
+fvdata=repmat(default_solid_color,[size(vol_vertex,1),1]);
+
+topo_flag_render=0;
+%2: curvature and overlay color
+if(~isempty(topo_value))
+    if(~iscell(topo_value))
+        ov=zeros(size(vol_vertex,1),1); 
+        ov(topo_vertex+1)=topo_value;
+        
+        topo_exclude=find(vol_vertex(:,3)<min(vol_vertex(topo_vertex+1,3)));
+
+        if(~isempty(topo_smooth))
+            %ovs=inverse_smooth('','value',ov,'step',topo_smooth,'face',vol_face','vertex',vol_vertex','flag_fixval',1);
+            ovs=inverse_smooth('','vertex',vol_vertex','face',vol_face','value',ov,'step',topo_smooth,'flag_fixval',0,'exc_vertex',topo_exclude,'inc_vertex',topo_include,'flag_regrid',topo_regrid_flag,'flag_regrid_zero',topo_regrid_zero_flag);
+        else
+            ovs=ov;
+        end;
+        
+        topo_flag_render=1;
+
+        if(~isempty(find(topo_value(:)>0))) topo_value_flag_pos=1; end;
+        if(~isempty(find(topo_value(:)<0))) topo_value_flag_neg=1; end;
+    else
+        ovs=[];
+        for h_idx=1:length(topo_value)
+            ov=zeros(size(vol_vertex_hemi{h_idx},1),1);
+            ov(topo_vertex{h_idx}+1)=topo_value{h_idx};
+            
+            if(~isempty(overlay_smooth))
+                %                ovs=cat(1,ovs,inverse_smooth('','vertex',vertex_coords_hemi{h_idx}','face',faces_hemi{h_idx}','value',ov,'step',overlay_smooth,'flag_fixval',0,'exc_vertex',overlay_exclude{h_idx},'inc_vertex',overlay_include{h_idx},'flag_regrid',overlay_regrid_flag,'flag_regrid_zero',overlay_regrid_zero_flag));
+                val=zeros(size(vol_vertex,1),1);
+                val(topo_vertex)=topo_value;
+                
+                ovs=inverse_smooth('','value',val,'step',topo_smooth,'face',vol_face','vertex',vol_vertex','flag_fixval',1);
+            else
+                ovs=cat(1,ovs,ov);
+            end;
+        end;
+        
+        topo_flag_render=1;
+        
+        if(~isempty(find(topo_value(:)>0))) topo_value_flag_pos=1; end;
+        if(~isempty(find(topo_value(:)<0))) topo_value_flag_neg=1; end;
+    end;
+    
+    if(isempty(topo_threshold))
+        tmp=sort(ovs(:));
+        topo_threshold=[tmp(round(length(tmp)*0.5)) tmp(round(length(tmp)*0.9))];
+    end;
+    c_idx=find(ovs(:)>=min(topo_threshold));
+    
+    fvdata(c_idx,:)=inverse_get_color(topo_cmap,ovs(c_idx),max(topo_threshold),min(topo_threshold));
+    
+    c_idx=find(ovs(:)<=-min(topo_threshold));
+    
+    fvdata(c_idx,:)=inverse_get_color(topo_cmap_neg,ovs(c_idx),-max(topo_threshold),-min(topo_threshold));
+end;
+
+
+h=patch('Faces',vol_face+1,'Vertices',vol_vertex,'FaceVertexCData',fvdata,'facealpha',alpha,'CDataMapping','direct','facecolor','interp','edgecolor','none');
+material dull;
+
+axis off vis3d equal;
+ 
+if(~isempty(topo_threshold))
+    set(gca,'climmode','manual','clim',topo_threshold);
+end;
+set(gcf,'color',bg_color);
+
+if(isempty(view_angle))
+   
+        view_angle=[-135 20];
+   end;
+view(view_angle(1), view_angle(2));
+% 
+% cp=campos;
+% cp=cp./norm(cp);
+% 
+% campos(1300.*cp);
+
+if(flag_camlight)
+    camlight(-90,0);
+    camlight(90,0);
+    camlight(0,0);
+    camlight(180,0);
+end;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%setup global object
+%%%%%%%%%%%%%%%%%%%%%%%%
+global etc_render_fsbrain;
+
+etc_render_fsbrain.brain_axis=gca;
+
+etc_render_fsbrain.faces=vol_face;
+etc_render_fsbrain.vertex_coords=vol_vertex;
+etc_render_fsbrain.faces_hemi=vol_face;
+etc_render_fsbrain.vertex_coords_hemi=vol_vertex;
+etc_render_fsbrain.fvdata=fvdata;
+etc_render_fsbrain.curv=[];
+%etc_render_fsbrain.curv_hemi=curv_hemi;
+etc_render_fsbrain.overlay_value=topo_value;
+
+etc_render_fsbrain.view_angle=view_angle;
+etc_render_fsbrain.bg_color=bg_color;
+etc_render_fsbrain.curv_pos_color=[];
+etc_render_fsbrain.curv_neg_color=[];
+etc_render_fsbrain.default_solid_color=default_solid_color;
+etc_render_fsbrain.alpha=alpha;
+
+etc_render_fsbrain.fig_brain=gcf;
+etc_render_fsbrain.fig_stc=[];
+etc_render_fsbrain.fig_gui=[];
+
+etc_render_fsbrain.overlay_value=topo_value;
+etc_render_fsbrain.overlay_stc=topo_stc;
+etc_render_fsbrain.overlay_stc_hemi=topo_stc_hemi;
+etc_render_fsbrain.overlay_stc_timeVec=topo_stc_timeVec;
+etc_render_fsbrain.overlay_stc_timeVec_unit=topo_stc_timeVec_unit;
+etc_render_fsbrain.overlay_stc_timeVec_idx=topo_stc_timeVec_idx;
+etc_render_fsbrain.overlay_stc_timeVec_idx_line=[];
+etc_render_fsbrain.overlay_stc_lim=topo_stc_lim;
+etc_render_fsbrain.overlay_vertex=topo_vertex;
+etc_render_fsbrain.overlay_smooth=topo_smooth;
+etc_render_fsbrain.overlay_threshold=topo_threshold;
+etc_render_fsbrain.overlay_cmap=topo_cmap;
+etc_render_fsbrain.overlay_cmap_neg=topo_cmap_neg;
+etc_render_fsbrain.overlay_value_flag_pos=topo_value_flag_pos;
+etc_render_fsbrain.overlay_value_flag_neg=topo_value_flag_neg;
+etc_render_fsbrain.overlay_exclude=topo_exclude;
+etc_render_fsbrain.overlay_include=topo_include;
+etc_render_fsbrain.flag_hold_fig_stc_timecourse=flag_hold_fig_stc_timecourse;
+etc_render_fsbrain.handleg_fig_stc_timecourse=[];
+etc_render_fsbrain.overlay_flag_render=topo_flag_render;
+        
+etc_render_fsbrain.h=h;
+etc_render_fsbrain.click_point=[];
+etc_render_fsbrain.click_vertex=[];
+etc_render_fsbrain.click_vertex_point=[];
+etc_render_fsbrain.click_overlay_vertex=[];
+etc_render_fsbrain.click_overlay_vertex_point=[];
+etc_render_fsbrain.roi_radius=[];
+
+etc_render_fsbrain.cluster_file=cluster_file;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%setup call-back function
+%%%%%%%%%%%%%%%%%%%%%%%%
+set(gcf,'WindowButtonDownFcn','etc_render_fsbrain_handle(''bd'')');
+set(gcf,'KeyPressFcn','etc_render_fsbrain_handle(''kb'')');
+set(gcf,'invert','off');
+
+hold on;
+
+if(flag_colorbar)
+    etc_render_fsbrain_handle('kb','c0','c0');
+end;
+return;
