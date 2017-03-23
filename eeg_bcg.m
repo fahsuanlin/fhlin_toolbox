b5775289d31309cb48d7e5fab431c788c8ea755c
fahsuanlin <fhlin@ntu.edu.tw>
2017-03-24 00:50:05 +0800

    032417

---
+++ eeg_bcg.m
@@ -0,0 +1,91 @@
+function [eeg_bcg, qrs_i_raw, bcg_all]=eeg_bcg(eeg,ecg,fs,varargin)
+
+%defaults
+BCG_tPre=0.1; %s
+BCG_tPost=0.6; %s
+flag_display=1;
+nsvd_bcg=3;
+
+for i=1:length(varargin)/2
+    option=varargin{i*2-1};
+    option_value=varargin{i*2};
+    switch lower(option)
+        case 'nsvd_bcg'
+            nsvd_bcg=option_value;
+        case 'bcg_tpre'
+            BCG_tPre=option_value;
+        case 'bcg_tpost'
+            BCG_tPost=option_value;
+        case 'flag_display'
+            flag_display=option_value;
+        otherwise
+            fprintf('unknown option [%s]...\n',option);
+            fprintf('error!\n');
+            return;
+    end;
+end;
+
+
+%----------------------------
+% BCG start;
+%----------------------------
+if(flag_display) fprintf('detecting EKG peaks...\n'); end;
+[qrs_amp_raw,qrs_i_raw,delay]=pan_tompkin(ecg,fs,0);
+
+
+BCG_tPre_sample=round(BCG_tPre.*fs);
+BCG_tPost_sample=round(BCG_tPost.*fs);
+
+eeg_bcg=eeg;
+
+non_ecg_channel=[1:size(eeg,1)];
+for ch_idx=1:length(non_ecg_channel)
+    if(flag_display) fprintf('*'); end;
+    
+    %generating BCG template
+    bcg_all{non_ecg_channel(ch_idx)}=[];
+    for trial_idx=1:length(qrs_i_raw)
+        if(((qrs_i_raw(trial_idx)-BCG_tPre_sample)>0)&((qrs_i_raw(trial_idx)+BCG_tPost_sample)<=size(eeg,2)))
+            bcg_all{non_ecg_channel(ch_idx)}=cat(1,bcg_all{non_ecg_channel(ch_idx)},eeg(non_ecg_channel(ch_idx),qrs_i_raw(trial_idx)-BCG_tPre_sample:qrs_i_raw(trial_idx)+BCG_tPost_sample));
+        end;
+    end;
+    [uu,ss,vv]=svd(bcg_all{non_ecg_channel(ch_idx)});
+    
+    bcg_approx=uu(:,1:nsvd_bcg)*ss(1:nsvd_bcg,1:nsvd_bcg)*vv(:,1:nsvd_bcg)';
+    bcg_residual=bcg_all{non_ecg_channel(ch_idx)}-bcg_approx;
+    
+    bcg_bases=vv(:,1:nsvd_bcg);
+    %bcg_bases=mean(bcg_all,1)';
+    
+    bcg_bases(:,end+1)=1; % confound
+    bcg_bases(:,end+1)=[1:size(bcg_bases,1)]'./size(bcg_bases,1); % confound
+    
+    bcg_proj_prep=inv(bcg_bases'*bcg_bases)*bcg_bases';
+    
+    
+    if(flag_display) fprintf('#'); end;
+    %ii=1;
+    for trial_idx=1:length(qrs_i_raw)
+        
+        if(((qrs_i_raw(trial_idx)-BCG_tPre_sample)>0)&((qrs_i_raw(trial_idx)+BCG_tPost_sample)<=size(eeg,2)))
+            y=eeg(non_ecg_channel(ch_idx),qrs_i_raw(trial_idx)-BCG_tPre_sample:qrs_i_raw(trial_idx)+BCG_tPost_sample)';
+            
+            beta=bcg_proj_prep*y;
+            y=y-bcg_bases(:,1:nsvd_bcg)*beta(1:nsvd_bcg);
+            eeg_bcg(non_ecg_channel(ch_idx),qrs_i_raw(trial_idx)-BCG_tPre_sample:qrs_i_raw(trial_idx)+BCG_tPost_sample)=y';
+            
+            
+            %eeg_bcg(non_ecg_channel(ch_idx),qrs_i_raw(trial_idx)-BCG_tPre_sample:qrs_i_raw(trial_idx)+BCG_tPost_sample)=bcg_residual(ii,:);
+            %bcg_correct_idx(trial_idx)=ii;
+            %ii=ii+1;
+        end;
+    end;
+end;
+
+if(flag_display) fprintf('\nBCG correction done!\n'); end;
+
+%----------------------------
+% BCG end;
+%----------------------------
+
+return;
---
+++ eeg_ga.m
@@ -0,0 +1,129 @@
+function [eeg_aas, ga_template, shift]=eeg_ga(eeg,eeg_trigger,TR,fs,varargin)
+
+%defaults
+flag_display=1;
+
+for i=1:length(varargin)/2
+    option=varargin{i*2-1};
+    option_value=varargin{i*2};
+    switch lower(option)
+        case 'flag_display'
+            flag_display=option_value;
+        otherwise
+            fprintf('unknown option [%s]...\n',option);
+            fprintf('error!\n');
+            return;
+    end;
+end;
+
+
+%----------------------------
+% AAS start;
+%----------------------------
+
+trigger=find(eeg_trigger);
+
+if(~isempty(TR))
+    n_samp=round(fs*TR);
+else
+    n_samp=[];
+end;
+
+%epoching; only valid for data with MRI triggers
+if(sum(abs(eeg_trigger))>0)
+    epoch=zeros(size(eeg,1),n_samp,length(trigger));
+    for tr_idx=1:length(trigger)
+        if(~isempty(n_samp))
+            if(trigger(tr_idx)+n_samp-1<=size(eeg,2))
+                epoch(:,:,tr_idx)=eeg(:,trigger(tr_idx):trigger(tr_idx)+n_samp-1);
+            end;
+        end;
+    end;
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % correction with interpolation
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %estimating shift
+    eeg_sum=squeeze(sum(eeg,1));
+    epoch_sum=zeros(n_samp,length(trigger));
+    for tr_idx=1:length(trigger)
+        if(~isempty(n_samp))
+            if(trigger(tr_idx)+n_samp-1<=length(eeg_sum))
+                epoch_sum(:,tr_idx)=eeg_sum(trigger(tr_idx):trigger(tr_idx)+n_samp-1);
+            end;
+        end;
+    end;
+    
+    ref_idx=1;
+    ref=epoch_sum(:,ref_idx);
+     
+    epoch_sum_shift=epoch_sum;
+    epoch_shift=epoch;
+    
+    if(flag_display) fprintf('GA: aligning trials...'); end;
+    shift(tr_idx)=0;
+    for tr_idx=2:length(trigger)
+        ss=[-5:1:5];
+        %ss=[-1:0.5:1];
+        dss=1;
+        if(flag_display) fprintf('*'); end;
+        for repeat_idx=1:4
+            D=[];
+            for ss_idx=1:length(ss)
+                D(:,ss_idx)=etc_circshift(ref(:),ss(ss_idx));
+            end;
+            
+            
+            cc=etc_corrcoef(epoch_sum(:,tr_idx),D);
+            [dummy,max_idx]=max(cc);
+            
+            shift(tr_idx)=ss(max_idx); %<-----number of sample shifted over trials!
+            
+            dss=dss./10;
+            if(max_idx>1)
+                if((max_idx+1)<length(ss))
+                    ss=[ss(max_idx-1):dss:ss(max_idx+1)];
+                else
+                    ss=[ss(max_idx-1):dss:ss(end)];
+                end;
+            else
+                if((max_idx+1)<length(ss))
+                    ss=[ss(1):dss:ss(max_idx+1)];
+                else
+                    ss=[ss(1):dss:ss(end)];
+                end;
+            end;
+        end;
+        
+        epoch_sum_shift(:,tr_idx)=etc_circshift(epoch_sum(:,tr_idx),-shift(tr_idx));
+        for ch_idx=1:size(epoch,1)
+            epoch_shift(ch_idx,:,tr_idx)=etc_circshift(squeeze(epoch(ch_idx,:,tr_idx)),-shift(tr_idx));
+        end;
+    end;
+    if(flag_display) fprintf('\n'); end;
+
+    if(flag_display) fprintf('estimating GA template...\n'); end;
+    ga_template=mean(epoch_shift,3);
+    
+    if(flag_display) fprintf('subtracting GA tamplate from data...'); end;
+    for tr_idx=1:length(trigger)
+        if(flag_display) fprintf('#'); end;
+        for ch_idx=1:size(epoch,1)
+            if(trigger(tr_idx)+n_samp-1<=size(eeg,2))
+                tmp=eeg(ch_idx,trigger(tr_idx):trigger(tr_idx)+n_samp-1);
+                tmp=tmp-etc_circshift(ga_template(ch_idx,:),shift(tr_idx));
+                eeg_aas(ch_idx,trigger(tr_idx):trigger(tr_idx)+n_samp-1)=tmp;
+            end;
+        end;
+    end;
+    if(flag_display) fprintf('\n'); end;
+end;
+
+
+%----------------------------
+% AAS end;
+%----------------------------
+
+return;
