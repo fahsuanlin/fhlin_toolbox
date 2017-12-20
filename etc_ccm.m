7e7d5c432dea7cb75051b350253c41f6c8e9a7f8
fahsuanlin <fhlin@ntu.edu.tw>
2017-12-21 02:21:46 +0800

    122117

---
+++ etc_ccm.m
@@ -0,0 +1,62 @@
+function [Y_est, rho, W, U, D]=etc_ccm(x,y,varargin)
+% etc_ccm convergent cross mapping
+%
+% calculate the y (source) cross mapped by x (target) (M_x)
+% y --> x gives high correlation coefficient (rho) between cross mapped y and y.
+%
+% fhlin@dec. 20 2017
+%
+
+flag_display=1;
+flag_graphics=1;
+
+tau=1; %interval between time series; sample
+E=1; %dimension of manifold
+
+for i=1:length(varargin)/2
+    option_name=varargin{i*2-1};
+    option_value=varargin{i*2};
+    
+    switch lower(option_name)
+        case 'e'
+            E=option_value;
+        case 'tau'
+            tau=option_value;
+        case 'flag_display'
+            flag_display=option_value;
+        case 'flag_graphics'
+            flag_graphics=option_value;
+        otherwise
+            fprintf('unknown option [%s].\nerror!\n',option_name);
+            return;
+    end;
+end;
+
+for time_idx=1:length(x)
+    t(:,time_idx)=[time_idx:tau:time_idx+(E-1)*tau]';
+end;
+mask=zeros(size(t));
+mask(find(t>length(x)))=1;
+idx_trim=find(sum(mask,1)>eps);
+t(:,idx_trim)=[];
+y_trim=y;
+y_trim(idx_trim)=[];
+X=x(t)';
+
+[IDX,D] = knnsearch(X,X,'K',E+2); %find the E+2 nearest neighbors, including itself.
+
+%find E+1 nearest neighbors by removing each point from itself 
+IDX(:,1)=[];
+D(:,1)=[];
+
+%weightings
+U=exp(-D./repmat(D(:,1),[1,size(D,2)]));
+W=U./repmat(sum(U,2),[1,size(U,2)]);
+
+Y=y(IDX); %<---locating the nearest points in Y manifold
+Y_est=reshape(sum(Y.*W,2),size(y_trim));
+
+rho=corrcoef(y_trim(:),Y_est(:));
+rho=rho(2,1);
+
+return;
--- etc_variability.m
+++ etc_variability.m
@@ -4,6 +4,7 @@ n_bstp=100;
 
 flag_display=1;
 
+flag_normalize=1;
 for i=1:length(varargin)/2
     option=varargin{i*2-1};
     option_value=varargin{i*2};
@@ -12,6 +13,8 @@ for i=1:length(varargin)/2
             n_bstp=option_value;
         case 'flag_display'
             flag_display=option_value;
+        case 'flag_normalize'
+            flag_normalize=option_value; %normalize the data to be zero-mean and unit-variance
         otherwise
             fprintf('unknown option [%s]\n! error!\n', option);
             return;
@@ -21,6 +24,10 @@ end;
 
 [n_trial,n_data]=size(data);
 
+if(flag_normalize)
+    data=bsxfun(@minus,data,mean(data,2));
+    data=bsxfun(@rdivide,data,std(data,0,2));
+end;
 %grand average
 %[u,s,v]=svd(data,'econ');
 %s=diag(s).^2;
@@ -29,7 +36,7 @@ end;
 %data=data-mean(data(:));
 %data=bsxfun(@minus,data,mean(data,2));
 avg=repmat(mean(data,1),[size(data,1) 1]);
-res=data(:)-avg(:)*(inv(avg(:)'*avg(:))*avg(:)'*data(:));
+res=data(:)-avg(:)*(inv(avg(:)'*avg(:))*(avg(:)'*data(:)));
 res=reshape(res,size(data));
 %res=bsxfun(@minus,data,mean(data,1));
 var_index=sum(res(:).^2)./sum(data(:).^2);
@@ -45,7 +52,7 @@ for bstp_idx=1:n_bstp
     %tmp=tmp-mean(tmp(:));
     %tmp=bsxfun(@minus,tmp,mean(tmp,2));
     avg=repmat(mean(tmp,1),[size(tmp,1) 1]);
-    res=tmp(:)-avg(:)*(inv(avg(:)'*avg(:))*avg(:)'*tmp(:));
+    res=tmp(:)-avg(:)*(inv(avg(:)'*avg(:))*(avg(:)'*tmp(:)));
     res=reshape(res,size(tmp));
     %res=bsxfun(@minus,tmp,mean(tmp,1));
     var_index_bstp(bstp_idx)=sum(res(:).^2)./sum(tmp(:).^2);
