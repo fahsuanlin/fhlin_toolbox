c65972239fba418a29a27bb3ac97d3f89d5728f1
fahsuanlin <fhlin@ntu.edu.tw>
2017-12-27 00:16:45 +0800

    122717

--- etc_ccm.m
+++ etc_ccm.m
@@ -14,6 +14,42 @@ function [y_est, rho, W, U, D]=etc_ccm(x,y,varargin)
 %
 % fhlin@dec. 20 2017
 %
+%
+% ----------------------------------------------------------------------
+% close all; clear all;
+% examples in Detecting Causality in Complex Ecosystems Sugihara et al.
+% Science (2012): DOI: 10.1126/science.1227079
+%
+% 
+% ll=[100:10:4000];
+% 
+% for l_idx=1:length(ll)
+%     d=zeros(ll(l_idx),2);
+%     d(1,:)=[0.2 0.4];
+%     for L=2:ll(l_idx)
+%         %figures 3c and 3d
+%         %d(L,1)=d(L-1, 1)*(3.7-3.7*d(L-1, 1)-0.0*d(L-1, 2));
+%         %d(L,2)=d(L-1,2)*(3.7-3.7*d(L-1, 2)-0.32*d(L-1, 1)); % x --> y
+%         
+%         %figures 1 and figure 3a
+%         d(L,1)=d(L-1, 1)*(3.8-3.8*d(L-1, 1)-0.02*d(L-1, 2));
+%         d(L,2)=d(L-1, 2)*(3.5-3.5*d(L-1, 2)-0.1*d(L-1, 1));
+%     end;
+%     
+%     
+%     [y_est, y_rho(l_idx)]=etc_ccm(d(:,1),d(:,2),'E',2);
+%     [x_est, x_rho(l_idx)]=etc_ccm(d(:,2),d(:,1),'E',2);
+%     
+% end;
+% 
+% 
+% plot(ll,x_rho,'b'); hold on;
+% plot(ll,y_rho,'r'); hold on;
+% 
+% return;
+% ----------------------------------------------------------------------
+
+
 y_est=[];
 rho=[];
 W=[];
