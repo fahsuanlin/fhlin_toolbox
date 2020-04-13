15481d8963edb22ca3c2a391058daba52aae3dfb
fahsuanlin <fhlin@sri.utoronto.ca>
2020-04-13 01:01:16 -0400

    041220

--- etc_trace_gui.m
+++ etc_trace_gui.m
@@ -22,7 +22,7 @@ function varargout = etc_trace_gui(varargin)
 
 % Edit the above text to modify the response to help etc_trace_gui
 
-% Last Modified by GUIDE v2.5 11-Apr-2020 20:27:13
+% Last Modified by GUIDE v2.5 12-Apr-2020 23:04:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -980,3 +980,23 @@ function listbox_view_style_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in checkbox_trigger_avg.
+function checkbox_trigger_avg_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox_trigger_avg (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox_trigger_avg
+global etc_trace_obj;
+
+etc_trace_obj.flag_trigger_avg=get(hObject,'Value');
+
+if(etc_trace_obj.flag_trigger_avg) %calculate average based on the current trigger
+    
+    
+else %restore the original un-averaged trace.
+
+
+end;
