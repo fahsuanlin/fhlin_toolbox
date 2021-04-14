fc5c498fe78e66d347a1b3b8f81a2b203a82d3ea
Fa-Hsuan Lin <fhlin@sri.utoronto.ca>
2021-04-14 02:39:58 -0400

    041420

--- codes/eeg_bcg_ccm3.m
+++ codes/eeg_bcg_ccm3.m
@@ -1,4 +1,4 @@
-function [eeg_bcg, qrs_i_raw]=eeg_bcg_ccm2(eeg,ecg,fs,varargin)
+function [eeg_bcg, qrs_i_raw]=eeg_bcg_ccm3(eeg,ecg,fs,varargin)
 
 %defaults
 flag_display=0;
@@ -12,6 +12,7 @@ n_ecg=[]; %search the nearest -n_ecg:+n_ecg; 10 is a good number; consider how t
 eeg_bcg=[];
 qrs_i_raw=[];
 
+t_pre=0.5; %s; time before QRS complex as the begnnning of one cardiac cycle
 for i=1:length(varargin)/2
     option=varargin{i*2-1};
     option_value=varargin{i*2};
@@ -28,6 +29,8 @@ for i=1:length(varargin)/2
             E=option_value;
         case 'n_ecg'
             n_ecg=option_value;
+        case 't_pre'
+            t_pre=option_value;
         otherwise
             fprintf('unknown option [%s]...\n',option);
             fprintf('error!\n');
@@ -69,7 +72,10 @@ non_ecg_channel=[1:size(eeg,1)];
 
 %get ECG indices
 ecg_idx=zeros(1,size(eeg,2)).*nan;
-ecg_idx(qrs_i_raw)=[1:length(qrs_i_raw)];
+%ecg_idx(qrs_i_raw)=[1:length(qrs_i_raw)];
+qrs_i_raw_shift=qrs_i_raw-round(t_pre*fs); %shift reference time point to -0.5-s before QRS
+qrs_i_raw_shift(find(qrs_i_raw_shift<0))=[];
+ecg_idx(qrs_i_raw_shift)=[1:length(qrs_i_raw_shift)];
 
 %figure; 
 %subplot(311); plot(ecg); set(gca,'xlim',[1 1000]);
@@ -178,10 +184,10 @@ for t_idx=1:size(eeg,2)
         U=exp(-ccm_D(t_idx,:)./ccm_D(t_idx,1));
         W=U./sum(U);
         
-        debug_ch=1;
+        debug_ch=12;
         for ch_idx=1:length(non_ecg_channel)
             %fprintf('*');
-            if(flag_display&&mod(t_idx,1000)==0&&t_idx==1000&&ch_idx==debug_ch)
+            if(flag_display&&mod(t_idx,1000)==0&&t_idx==35000&&ch_idx==debug_ch)
                 figure(1); clf;
                 subplot(121); hold on;
                 h=plot(ecg); 
@@ -307,7 +313,7 @@ for t_idx=1:size(eeg,2)
                 ylabel('EKG(t+\tau) (a.u.)');
                 etc_plotstyle;
 
-                if(t_idx==2000) keyboard; end;
+                if(t_idx==35000) keyboard; end;
                    
                 pause(0.01);
             end;
@@ -315,205 +321,86 @@ for t_idx=1:size(eeg,2)
     end;
 end;
 
-eeg_bcg=eeg-eeg_bcg_pred;
-
-if(flag_display) fprintf('BCG CCM correction done!\n'); end;
-
-
-return;
-        
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%prepare search.......
-ccm_IDX=zeros(size(eeg,2),nn);
-ccm_D=zeros(size(eeg,2),nn);
-
-time_idx=[1:size(eeg,2)];
-for dim_idx=1:E
-    t0(dim_idx,:)=[1:size(eeg,2)]+(dim_idx-1).*tau+round(fs.*delay_time);
-end;
-exclude_mask=zeros(size(t0));
-exclude_mask(find(t0>size(eeg,2)))=1;
-exclude_mask(find(t0<1))=1;
-
-t0(:,find(sum(exclude_mask,1)>eps))=[];
-
-time_idx(find(sum(exclude_mask,1)>eps))=nan;
-
-X0=ecg(t0)';
-%tmp=eeg(non_ecg_channel(ch_idx),:);
-%Y0=tmp(t0)';
-
-%<---the most time-consuming part!! ----->
-% global search takes too long for long time series!!
-%[IDX,D] = knnsearch(X0,X0,'K',round((E+1)*fs*1.2));
-%<---the most time-consuming part!! ----->
-
-%search preparation DONE!
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-knn_idx=0;
-for t_idx=1:size(eeg,2)
-    if(isnan(time_idx(t_idx)))
-        
-    else
-        if(flag_display) if(mod(t_idx,1000)==0) fprintf('[%d|%d]...\r',t_idx,size(eeg,2)); end; end;
-        ecg_idx_now=ecg_idx(t_idx);
-        ecg_idx_buffer=ecg_idx; %<----you can limit the search range in time here
-        
-        ecg_phase_percent_now=ecg_phase_percent(t_idx);
-        ecg_phase_percent_buffer=ecg_phase_percent;
-        
-        phase_limit=5; %+/-5% of an ECG cycle
-        phase_idx=find((angle(exp(sqrt(-1).*(ecg_phase_percent_buffer-ecg_phase_percent_now)./100.*2.*pi))*100/2/pi<=phase_limit)&(angle(exp(sqrt(-1).*(ecg_phase_percent_buffer-ecg_phase_percent_now)./100.*2.*pi))*100/2/pi>=-phase_limit));
-        
-        ecg_count_limit=n_ecg; %-10:10 ecg included
-        ecg_count_idx=find(abs(ecg_idx_buffer-ecg_idx_now)<=ecg_count_limit);
-        ecg_count_idx(find(ecg_idx_buffer(ecg_count_idx)==ecg_idx_now))=[];
-        
-        knn_idx=intersect(ecg_count_idx,phase_idx);
-        
-        %exclude search from time points outside the manifold range
-        knn_idx(find(isnan(time_idx(knn_idx))))=[];
-        
-        
-        [IDX,D] = knnsearch(X0(knn_idx,:),X0(t_idx,:),'K',nn);
-        
-        ccm_IDX(t_idx,:)=knn_idx(IDX(1,1:end));
-        ccm_D(t_idx,:)=D(1,1:end);
-        
-        %cross mapping
-        if(ccm_D(t_idx,1)<eps) ccm_D(t_idx,1)=eps; end;
-        U=exp(-ccm_D(t_idx,:)./ccm_D(t_idx,1));
-        W=U./sum(U);
-        
-        
-        %     ecg_idx_buffer(find(ecg_idx_buffer==ecg_idx_now))=nan;
-        %
-        %     if(knn_idx~=ecg_idx_now)
-        %         if(flag_display)
-        %             fprintf('#');
-        %         end;
-        %
-        %         knn_begin_idx=[];
-        %         knn_end_idx=[];
-        %
-        %         if(ecg_idx_now-n_ecg>0)
-        %             knn_begin_idx=min(find(ecg_idx==ecg_idx_now-n_ecg));
-        %         else
-        %             knn_begin_idx=1;
-        %
-        %             ecg_idx_begin=ecg_idx(1);
-        %             if(ecg_idx_begin+2*n_ecg<=max(ecg_idx))
-        %                 knn_end_idx=max(find(ecg_idx==ecg_idx_begin+2*n_ecg));
-        %                 if(knn_end_idx>size(X0,1)) knn_end_idx=size(X0,1); end;
-        %             else
-        %                 knn_end_idx=size(X0,1);
-        %             end;
-        %         end;
-        %
-        %         if(isempty(knn_end_idx))
-        %         if(ecg_idx_now+n_ecg<=max(ecg_idx))
-        %             knn_end_idx=max(find(ecg_idx==(ecg_idx_now+n_ecg)));
-        %             if(knn_end_idx>size(X0,1)) knn_end_idx=size(X0,1); end;
-        %         else
-        %             knn_end_idx=size(X0,1);
-        %
-        %             ecg_idx_end=ecg_idx(end);
-        %             if(ecg_idx_end-2*n_ecg>0)
-        %                 knn_begin_idx=min(find(ecg_idx==ecg_idx_end-2*n_ecg));
-        %             else
-        %                 knn_begin_idx=1;
-        %             end;
-        %         end;
-        %         end;
-        %
-        %
-        %         %<---the most time-consuming part!! ----->
-        %         [IDX,D] = knnsearch(X0(knn_begin_idx:knn_end_idx,:),X0(knn_begin_idx:knn_end_idx,:),'K',knn_end_idx-knn_begin_idx+1);
-        %         %<---the most time-consuming part!! ----->
-        %
-        %         knn_idx=ecg_idx_now;
-        %     end;
-        %
-        %     if(isnan(time_idx(t_idx)))
-        %
-        %     else
-        %
-        %
-        %         idx_buffer=IDX(t_idx-(knn_begin_idx-1),:)+(knn_begin_idx-1);
-        %         [C,ia,ic]=unique(ecg_idx(idx_buffer),'stable');
-        %         ccm_IDX(t_idx,:)=IDX(t_idx-(knn_begin_idx-1),ia(2:nn+1));
-        %         ccm_D(t_idx,:)=D(t_idx-(knn_begin_idx-1),ia(2:nn+1));
-        %
-        %         %cross mapping
-        %         U=exp(-ccm_D(t_idx,:)./ccm_D(t_idx,1));
-        %         W=U./sum(U);
-        %
-        
-        
+suppress_method=3;
+switch(suppress_method)
+    case 1 % direct subtraction
+        eeg_bcg=eeg-eeg_bcg_pred;
+    case 2 %regression
+        eeg_bcg=eeg;
+        eeg_bcg(:)=nan;
+        try
         for ch_idx=1:length(non_ecg_channel)
-            %fprintf('*');
-            if(flag_display&&mod(t_idx,1000)==0&&t_idx==1000)
-                %if(t_idx==1)
-                    figure(1); clf;
-                    subplot(121);
-                    plot(ecg); hold on;
-                    set(gca,'xlim',[1 5e3]);
-                    subplot(122);
-                    plot(eeg(non_ecg_channel(ch_idx),:)); hold on;
-                    set(gca,'xlim',[1 5e3]);
-                    
-                    figure(2); clf;
-                    plot(eeg(non_ecg_channel(ch_idx),:)); hold on;
-                    set(gca,'xlim',[1 5e3]);
-                    
-                    ha=[];
-                    h1=[];
-                    hb=[];
-                    h2=[];
-                %end;
+            for idx=1:length(ecg_onset_idx)-1
+                bcg_now=eeg_bcg_pred(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                if(sum(abs(bcg_now))>eps)
+                    eeg_now=eeg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                    D=cat(2,ones(length(bcg_now(:)),1),bcg_now(:)); %DC offset + BCG for regression
+                    beta=inv(D'*D)*D'*eeg_now(:);
+                    eeg_bcg_corr_now=eeg_now(:)-D(:,2)*beta(2); %remove only BCG part
+                    eeg_bcg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx))=eeg_bcg_corr_now(:)';
+                else
+                    eeg_bcg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx))=eeg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                end;
             end;
-            
-            tic;
-            
-            %get estimates by cross mapping
-            eeg_bcg_pred(non_ecg_channel(ch_idx),t_idx)=eeg(non_ecg_channel(ch_idx),ccm_IDX(t_idx,:))*W';
-            
-            if(flag_display&&mod(t_idx,1000)==0)
-                figure(1);
-                subplot(121);
-                xx=cat(1,t_idx,ccm_IDX(t_idx,:)');
-                if(~isempty(ha)) delete(ha); end;
-                if(~isempty(h1)) delete(h1); end;
-                
-                ha=plot(xx,ecg(xx),'ro');
-                h1=plot(xx(1),ecg(xx(1)),'go');
-                
-                
-                subplot(122);
-                %plot(t_idx,y_m(t_idx),'r.');
-                %set(gca,'xlim',[1 1000]);
-                if(~isempty(hb)) delete(hb); end;
-                if(~isempty(h2)) delete(h2); end;
-                hb=plot(xx,eeg(xx),'ro');
-                h2=plot(xx(1),eeg(xx(1)),'go');
-                
-                figure(2);
-                plot(eeg_bcg_pred(non_ecg_channel(ch_idx),1:t_idx),'r');
-                %set(gca,'xlim',[knn_begin_idx knn_end_idx]);
-                set(gca,'xlim',[1 5e3]);
-                
-                pause(0.01);
+            if(length(find(isnan(eeg_bcg(non_ecg_channel(ch_idx),:))))>0)
+                keyboard;
             end;
         end;
-    end;
+        catch ME
+                fprintf('error in BCG regression...\n');
+        end;
+    case 3 %regression with end-point anchoring
+        eeg_bcg=eeg;
+        eeg_bcg(:)=nan;
+        try
+        for ch_idx=1:length(non_ecg_channel)
+            for idx=1:length(ecg_onset_idx)-1
+                bcg_now=eeg_bcg_pred(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                if(sum(abs(bcg_now))>eps)
+                    %figure(1); hold on;
+                    %plot(eeg(non_ecg_channel(ch_idx),1:ecg_offset_idx(idx)),'k');
+                    
+                    eeg_now=eeg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                    bnd0=[eeg_now(1) eeg_now(end)];
+                    
+                    D=cat(2,ones(length(bcg_now(:)),1),bcg_now(:)); %DC offset + BCG for regression
+                    beta=inv(D'*D)*D'*eeg_now(:);
+                    eeg_bcg_corr_now=eeg_now(:)-D(:,:)*beta; %remove only BCG part
+                    
+                    eeg_bcg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx))=eeg_bcg_corr_now(:)';
+                    %plot(eeg_bcg(non_ecg_channel(ch_idx),1:ecg_offset_idx(idx)),'r');
+                    %plot(eeg_bcg_pred(non_ecg_channel(ch_idx),1:ecg_offset_idx(idx)),'g');
+                    bnd1=[eeg_bcg_corr_now(1) eeg_bcg_corr_now(end)];
+
+                    bcg_bnd_bases=zeros(length(eeg_bcg_corr_now),2);
+                    bcg_bnd_bases(:,1)=1; % confound
+                    bcg_bnd_bases(:,2)=[1:length(eeg_bcg_corr_now)]'./length(eeg_bcg_corr_now); % confound
+                    
+                    eeg_bcg_corr_now=eeg_bcg_corr_now-bcg_bnd_bases*inv(bcg_bnd_bases([1,end],:)'*bcg_bnd_bases([1,end],:))*(bcg_bnd_bases([1,end],:)'*(bnd1-bnd0)');
+                    
+                    eeg_bcg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx))=eeg_bcg_corr_now(:)';
+                    %plot(eeg_bcg(non_ecg_channel(ch_idx),1:ecg_offset_idx(idx)),'b');
+
+                    
+                    %keyboard;
+                else
+                    eeg_bcg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx))=eeg(non_ecg_channel(ch_idx),ecg_onset_idx(idx):ecg_offset_idx(idx));
+                end;
+            end;
+            if(length(find(isnan(eeg_bcg(non_ecg_channel(ch_idx),:))))>0)
+                keyboard;
+            end;
+        end;
+        catch ME
+                fprintf('error in BCG regression...\n');
+        end;
+        
+        
 end;
-
-eeg_bcg=eeg-eeg_bcg_pred;
-
 if(flag_display) fprintf('BCG CCM correction done!\n'); end;
 
-%----------------------------
+
+return;%----------------------------
 % BCG CCM end;
 %----------------------------
 
