54725993bb2af8b7fb50616c77e6b1d6d88c38dd
Fa-Hsuan Lin <fhlin@sri.utoronto.ca>
2023-12-28 17:00:22 -0500

    122823

--- codes/etc_onset_adjust.m
+++ codes/etc_onset_adjust.m
@@ -46,7 +46,7 @@ template=[];
 for t_idx=1:length(onsets_orig)-1
 %    tmp=signal(onsets_orig(t_idx):onsets_orig(t_idx)+duration_min-1);
     tmp_idx=onsets_orig(t_idx)-round(duration_median/2):onsets_orig(t_idx)+round(duration_median/2);
-    if(isempty(find(tmp_idx<0))&&isempty(find(tmp_idx>length(signal))))
+    if(isempty(find(tmp_idx<1))&&isempty(find(tmp_idx>length(signal))))
        tmp=signal(tmp_idx);
        template(:,t_counter)=tmp(:);
        t_counter=t_counter+1;
